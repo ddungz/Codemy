da02628a3be1600a32bea8025a0ac9489be4ae6f
ddungz <ddungz@users.noreply.github.com>
2017-06-07 18:07:10 +0900

    Update Register.java

--- app/src/main/java/com/updatingtech/codemy/Register.java
+++ app/src/main/java/com/updatingtech/codemy/Register.java
@@ -114,7 +114,6 @@ public class Register extends Activity implements View.OnClickListener {
                 if(!validate())
                     Toast.makeText(getBaseContext(), "Enter your infor", Toast.LENGTH_SHORT).show();
                 //new HttpAsyncTask().execute("http://localhost:8080/user/register");
-                new HttpAsyncTask().execute("http://hmkcode.appspot.com/jsonservlet");
                 System.out.print("registerBTN clicked");
                 Log.i("CLICKED", "register button clicked");
                 Toast.makeText(getBaseContext(), "You've just clicked Register btn", Toast.LENGTH_SHORT).show();
